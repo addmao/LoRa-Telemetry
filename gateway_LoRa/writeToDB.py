e9158ddc8cb60b027088217de9fcbd60cd33e689
Add Mao <put_1996@live.com>
2019-06-16 20:40:32 +0700

    update insertion of new data to database

--- gateway_LoRa/writeToDB.py
+++ gateway_LoRa/writeToDB.py
@@ -1,3 +1,4 @@
+import sys
 import serial
 #import os
 import time
@@ -14,15 +15,21 @@ def insertDataToDB(data_read):
 		#rssi_data = 0;
 		print data_read
 		
-		water_level_data = data_read.split(",")[3]
-		temp_data = data_read.split(",")[0]
-		humid_data = data_read.split(",")[1]
-		rssi_data = data_read.split(",")[2]
-		
-		print "water level" + water_level_data
-		print "temp_data" + temp_data
-		print "humid_data" + humid_data
-		print "rssi_data" + rssi_data
+		temp_data = data_read.split(",")[0] // TEMP
+		humid_data = data_read.split(",")[1] // HUMIDITY
+		rssi_data = data_read.split(",")[2] // RSSI
+		snr_data = data_read.split(",")[3] // SNR
+		water_level_data = data_read.split(",")[4] // WATER LEVEL
+		water_temp_data = data_read.split(",")[5] // WATER TEMP
+		voltage_data = data_read.split(",")[6] // VOLTAGE
+
+		print "Water Level: " + water_level_data
+		print "Temperature Water: " + water_temp_data
+		print "Voltage Probe: " + voltage_data
+		print "Temperate Box: " + temp_data
+		print "Humidity Box: " + humid_data
+		print "RSSI: " + rssi_data
+		print "SNR: " + snr_data
 
 		#if(data_read < 0): 
 		#	rssi_data = data_read
@@ -31,7 +38,7 @@ def insertDataToDB(data_read):
 		
 		datetimeWrite = (time.strftime("%Y-%m-%d ") + time.strftime("%H:%M:%S"))
 		#sql_query = ("""INSERT INTO water_level_data (datetime,water_level,rssi) VALUES (%s,%s,%s)""",(datetimeWrite,water_level_data,rssi_data))
-		sql_query = ("""INSERT INTO monitoring_system (water_level, temp, humid, rssi, datetime) VALUES (%s, %s, %s, %s, %s)""",(water_level_data, temp_data, humid_data, rssi_data, datetimeWrite))
+		sql_query = ("""INSERT INTO monitoring_system (datetime, temp, humid,  rssi, snr, water_level, water_temp, voltage) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)""",(datetimeWrite, temp_data, humid_data, rssi_data, snr_data, water_level_data, water_temp_data, voltage_data))
 		
 		try:
         		print "Writing to database..."
@@ -60,6 +67,7 @@ def main():
                 if data_read != '':
 			#print(data_read)
 			insertDataToDB(data_read)
+                sys.stdout.flush()
                 time.sleep(5)
 	#insertDataToDB(data_read)
 
